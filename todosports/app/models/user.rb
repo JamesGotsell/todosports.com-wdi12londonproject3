89500df6ca614257aa96f2718914a335b8c82d62
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-25 15:03:56 +0100

    added name field to devise registration and edit

--- todosports/app/models/user.rb
+++ todosports/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :events
   validates_associated :events
-  validates :name, length: { minimum: 2 }
-  validates :age, numericality: true
+  # validates :name, length: { minimum: 2 }
+  # validates :age, numericality: true
   
 end
