7b8bda6295d78d062e4a228b2b965ff17501aaa3
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-25 13:51:42 +0100

    added config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } to development.rb

--- todosports/config/routes.rb
+++ todosports/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   resources :users
+  root to: "home#index"
   # config/initializers/high_voltage.rb
   HighVoltage.configure do |config|
     config.home_page = 'home'
