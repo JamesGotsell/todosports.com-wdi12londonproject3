d6f5237a68287d3dee57dfad152572e064bd3c3c
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-25 14:01:31 +0100

    migrated devise to users

--- todosports/config/routes.rb
+++ todosports/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
-  resources :users
-  root to: "home#index" 
+  # resources :users
+  # root to: "home#index" 
   # config/initializers/high_voltage.rb
   HighVoltage.configure do |config|
     config.home_page = 'home'
