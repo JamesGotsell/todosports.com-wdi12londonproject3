86764e0d483613904e9fd22e77a449925b28e228
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-25 16:57:57 +0100

    implemented rails admin

--- todosports/db/seeds.rb
+++ todosports/db/seeds.rb
@@ -6,4 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user1 = User.create({ name: "james" , age: 27, picture: nil , email: "james@james.com", password: "password" admin: true})
+user1 = User.create({ name: "james" , age: 27, picture: nil , email: "james@james.com", password: "password", admin: true})
+user1 = User.create({ name: "bob" , age: 29, picture: nil , email: "bob@bob.com", password: "password", admin: false})
+user1 = User.create({ name: "jack" , age: 27, picture: nil , email: "jack@jack.com", password: "password", admin: false})
