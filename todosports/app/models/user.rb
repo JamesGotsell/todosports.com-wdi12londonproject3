73b8aa881092ac964d01cc02f6803cf7b513f6f2
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-25 13:23:41 +0100

    added one user into the seed data

--- todosports/app/models/user.rb
+++ todosports/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :events
+  validates_associated :events
+  validates :name, length: { minimum: 2 }
+  validates :age, numericality: true
+  
 end
--- todosports/db/seeds.rb
+++ todosports/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.create({ name: "james" , age: 27, picture: nil })
