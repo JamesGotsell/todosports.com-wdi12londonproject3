b89ce4428a9d6d648de206fece3a68c5fac5896b
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-26 15:05:29 +0100

    underscore, jquery and backbone are included

--- todosports/app/controllers/application_controller.rb
+++ todosports/app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
       render text: "", layout: true
     end
     
-
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
