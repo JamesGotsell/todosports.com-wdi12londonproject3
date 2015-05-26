5f41ccffe0810a7f97c8c7a3d9d804f639d40db6
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-26 14:47:30 +0100

    def index     render text: , layout: true   end...to the apllication controller

--- todosports/app/controllers/application_controller.rb
+++ todosports/app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+      render text: "", layout: true
+    end
+    
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -14,4 +19,11 @@ class ApplicationController < ActionController::Base
         u.permit(:email, :password, :password_confirmation, :current_password, :picture)
       end
     end
+
+    # # this returns to the root if not a ajex request 
+    #   before_action :redirect_if_not_xhr
+    # private
+    #   def redirect_if_not_xhr 
+    #     redirect_to root_path if (!request.xhr? && request.env['PATH_INFO'] != root_path)
+    #   end
 end
--- todosports/config/routes.rb
+++ todosports/config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # config/initializers/high_voltage.rb
   HighVoltage.configure do |config|
     config.home_page = 'home'
+    
   end
+  
 end
