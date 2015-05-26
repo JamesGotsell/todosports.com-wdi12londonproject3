f93207927ea699d9659fd2e9358f89121c526449
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-26 10:13:18 +0100

    sorted carrierwave

--- todosports/app/models/user.rb
+++ todosports/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :events
-  validates_associated :events
+  # validates_associated :events
   # validates :name, length: { minimum: 2 }
   # validates :age, numericality: true
 
