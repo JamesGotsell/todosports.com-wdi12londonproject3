6e0389a9cbcffe0f4b919f009ea3b68fbae2ad12
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-29 17:03:15 +0100

    added two click functions to events.js to click through the pages

--- app/assets/javascripts/views/events.js
+++ app/assets/javascripts/views/events.js
@@ -15,13 +15,39 @@ define([
       var self = this;
       var collection = new Backbone.PageableCollection();
         collection.getPage(1).done(function(data){
-          self.render.data
+          self.render(data)
           })
     },
     render: function(data) {
       var template = _.template(EventsTemplate);
       this.$el.html(template({events: data}));
       return this.el;
+    },
+
+    events: {
+       events: {
+        "click .next_page": "nextPage", 
+        "click .previous_page": "previousPage"
+      },
+
+      nextPage: function{
+        event.preventDefault();
+        var self = this;
+        var collection = new Backbone.PageableCollection();
+        collection.getnext().done(function(data){
+          self.render(data)
+        })
+      }, 
+      previousPage: function{
+        event.preventDefault();
+        var self = this; 
+        // how to recall the previous page
+        var collection = new Backbone.PageableCollection();
+        collection.getPreviousPage().done(function(data){
+          self.render(data)
+        })
+      }
+
     }
   });
 
