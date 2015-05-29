c644a2cfea40e6ae082b69c20a7466f4113fa897
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-29 10:11:29 +0100

    Removing the eventful gem

--- config/EventfulApi.rb
+++
@@ -1,5 +0,0 @@
-EventfulApi.configure do |config|
-  config.application_key = ENV['EVENTFUL_API_KEY']
-  config.consumer_key    = ENV['EVENTFUL_API_CONSUMER_KEY']
-  config.consumer_secret = ENV['EVENTFUL_API_CONSUMER_SECRET']
-end
