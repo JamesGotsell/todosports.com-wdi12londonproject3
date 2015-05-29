505a9aa90233b0e0b1d8652de875b7616699330e
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-29 10:04:47 +0100

    Legacy views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,19 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
+require.config({
+  paths: {
+    jquery: 'lib/jquery',
+    jquery_ujs: 'jquery_ujs',
+    underscore: 'lib/underscore',
+    backbone: 'lib/backbone',
+    text: 'lib/text'
+  }
+});
 
-//= require_tree .
+require([
+  'app',
+  'jquery',
+  'jquery_ujs'
+], function(App, jquery, jquery_ujs){
+  if ($('main').length > 0) {
+    App.initialize();
+  }
+});
