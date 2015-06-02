3b4eb4b710e59c94fe8c7a0e9e1f0a9eaf0d9c84
JamesGotsell <j.e.gotsell@gmail.com>
2015-06-02 23:17:47 +0100

    working on styling

--- app/assets/javascripts/views/myevents.js
+++ app/assets/javascripts/views/myevents.js
@@ -22,7 +22,16 @@ define([
       var template = _.template(myEventsTemplate);
       this.$el.html(template({ favourites: data.models }));
       return this.el;
+    }, 
+    events: {
+      "click .add btn" : "buyTicket"
+    },
+
+    buyTicket: function(){
+      console.log("clicked")
+      alert("you have bought a ticket for this event")
     }
+
   });
 
   return myEventsView;
