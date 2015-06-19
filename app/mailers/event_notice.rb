9514d54168e60f1b187b7281bbb6507dba62c4df
JamesGotsell <j.e.gotsell@gmail.com>
2015-06-19 13:38:06 +0100

    uncommented config.action_mailer.default_url_options = {host: localhost , port: 300

--- app/mailers/event_notice.rb
+++ app/mailers/event_notice.rb
@@ -1,8 +1,8 @@
 class EventNotice < Devise::Mailer
   default template_path: 'devise/mailer'
   def event_reminder
-      @user = user 
-      mail(to: @user.email, subject: "Your Events")
+      # @user = user 
+      # mail(to: @user.email, subject: "Your Events")
   end
 
 end
